1d29d4469a1307ba7489029159955064a1a3561f
GMaikerYactayo <maiker.gonzales@vallegrande.edu.pe>
2023-07-10 01:24:29 -0500

    eureka

--- bill-service/src/main/java/com/bill/service/BillServiceApplication.java
+++ bill-service/src/main/java/com/bill/service/BillServiceApplication.java
@@ -2,11 +2,13 @@ package com.bill.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.bill.service")
 public class BillServiceApplication {
 
--- pay-mode-service/src/main/java/com/pay/mode/service/PayModeServiceApplication.java
+++ pay-mode-service/src/main/java/com/pay/mode/service/PayModeServiceApplication.java
@@ -2,8 +2,10 @@ package com.pay.mode.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PayModeServiceApplication {
 
 	public static void main(String[] args) {
